abebae70f37c34ee038692eba8c5f03ce4500b4b
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-08-18 11:44:25 -0300

    adicionado costante para o action type

--- src/store/actions/numerosActions.js
+++ src/store/actions/numerosActions.js
@@ -1,16 +1,17 @@
 // Action creator = retorna  uma ação
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from "./actionTypes";
 
-export function alterarNumeroMinimo(novoNumero){
-    return {
-        type:'NUM_MIN_ALTERADO',
-        payload: novoNumero
-    }
+export function alterarNumeroMinimo(novoNumero) {
+  return {
+    type: NUM_MIN_ALTERADO,
+    payload: novoNumero,
+  };
 }
 
 // action creator para criação do numero máximo
-export function alterarNumeroMaximo(novoNumero){
-    return {
-        type:'NUM_MAX_ALTERADO',
-        payload: novoNumero
-    }
+export function alterarNumeroMaximo(novoNumero) {
+  return {
+    type: NUM_MAX_ALTERADO,
+    payload: novoNumero,
+  };
 }
--- src/store/reducers/numerosReducer.js
+++ src/store/reducers/numerosReducer.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from "../actions/actionTypes";
 
 const initialState = {
     min: 1,
@@ -7,12 +8,12 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-      case "NUM_MIN_ALTERADO":
+      case NUM_MIN_ALTERADO:
         return {
           ...state,
           min: action.payload,
         };
-      case "NUM_MAX_ALTERADO":
+      case NUM_MAX_ALTERADO:
         return {
           ...state,
           max: action.payload,
